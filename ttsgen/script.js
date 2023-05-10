dd70df5bcbe2dfa2c8963a40f03635d7879a3e8b
ZehJulianLp <begemann.julian@gmail.com>
2023-05-10 17:30:55 +0200

    IT WORKS!

--- ttsgen/script.js
+++ ttsgen/script.js
@@ -1,40 +1,42 @@
-const synth = window.speechSynthesis;
+(() => {
+  const synth = window.speechSynthesis;
 
-const inputForm = document.getElementById("input-form");
-const inputTxt = document.getElementById("text-input"); // Update ID to "text-input"
-const voiceSelect = document.getElementById("voice-select");
+  const inputForm = document.getElementById("input-form");
+  const inputTxt = document.getElementById("text-input"); // Update ID to "text-input"
+  const voiceSelect = document.getElementById("voice-select");
 
-let voices = [];
+  let voices = [];
 
-function populateVoiceList() {
+  function populateVoiceList() {
   voices = synth.getVoices();
   voiceSelect.innerHTML = "";
   voices.forEach((voice) => {
-    const option = document.createElement("option");
-    option.textContent = voice.name + " (" + voice.lang + ")";
-    option.setAttribute("data-lang", voice.lang);
-    option.setAttribute("data-name", voice.name);
-    voiceSelect.appendChild(option);
+      const option = document.createElement("option");
+      option.textContent = voice.name + " (" + voice.lang + ")";
+      option.setAttribute("data-lang", voice.lang);
+      option.setAttribute("data-name", voice.name);
+      voiceSelect.appendChild(option);
   });
-}
+  }
 
-if (synth.onvoiceschanged !== undefined) {
+  if (synth.onvoiceschanged !== undefined) {
   synth.onvoiceschanged = populateVoiceList;
-} else {
+  } else {
   populateVoiceList();
-}
+  }
 
-inputForm.addEventListener("submit", (event) => {
+  inputForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const utterThis = new SpeechSynthesisUtterance(inputTxt.value);
   const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
   for (let i = 0; i < voices.length; i++) {
-    if (voices[i].name === selectedOption) {
+      if (voices[i].name === selectedOption) {
       utterThis.voice = voices[i];
       break;
-    }
+      }
   }
   synth.speak(utterThis);
   inputTxt.blur();
-});
+  });
+})();
